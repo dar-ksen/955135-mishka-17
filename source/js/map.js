6f57f44166f223dfa2ba8cbdbd878f73c9bbc912
Dar <ksen.vl.set@gmail.com>
2019-07-28 19:28:59 +0300

    Добавил google map через api key

---
+++ source/js/map.js
@@ -0,0 +1,17 @@
+var map;
+var image = 'img/icon-map-pin.svg'
+function initMap() {
+  var coordinates = {lat: 59.9387782, lng: 30.3231967};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: coordinates,
+    zoom: 18
+  });
+
+  marker = new google.maps.Marker({
+    position: coordinates,
+    map: map,
+    icon: image,
+    animation: google.maps.Animation.DROP
+  });
+}
