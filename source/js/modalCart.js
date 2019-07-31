a6318c70cfb1ef9f925d223090146f472a44b0a4
Dar <ksen.vl.set@gmail.com>
2019-07-31 16:18:29 +0300

    Исправлен js. Подключен picturefill.js

--- source/js/modalCart.js
+++ source/js/modalCart.js
@@ -1,46 +1,55 @@
-`use strict`;
+'use strict';
+const escCode = 27;
+
 var modalCart = document.querySelector('.modal-cart');
 var mist = document.querySelector('.mist');
+var openModalCart = document.querySelector('.week-goods__btn');
+var cards = document.querySelector('.card-product');
 
-mist.addEventListener('click', function () {
+var modalOpen = function () {
+  modalCart.classList.add('modal-cart--open');
+  mist.classList.add('mist--open');
+}
+
+var modalClose = function () {
   modalCart.classList.remove('modal-cart--open');
   mist.classList.remove('mist--open');
+}
+
+mist.addEventListener('click', function () {
+  modalClose();
 });
 
 window.addEventListener('keydown', function (evt){
-  if (evt.keyCode === 27) {
+  if (evt.keyCode === escCode) {
     evt.preventDefault();
     if (modalCart.classList.contains('modal-cart--open')) {
-      modalCart.classList.remove('modal-cart--open');
-      mist.classList.remove('mist--open');
+      modalClose();
     }
   }
 });
 
 //main
-if (document.querySelector('.week-goods__btn')) {
-  var openModalCart = document.querySelector('.week-goods__btn');
+console.log(openModalCart);
+if (openModalCart) {
   openModalCart.addEventListener('click', function (evt) {
     evt.preventDefault();
-    modalCart.classList.add('modal-cart--open');
-    mist.classList.add('mist--open');
+    modalOpen();
   });
 }
 
 //catalog
-if (document.querySelector('.card-product')) {
-  var carts = document.querySelector('.card-product');
-
-  carts.onclick = function(evt) {
+console.log(cards);
+if (cards) {
+  cards.addEventListener('click', function (evt) {
     var target = evt.target;
-    while (target != carts) {
+    while (target != cards) {
       if (target.classList.contains('card-product__btn')) {
         evt.preventDefault();
-        modalCart.classList.add('modal-cart--open');
-        mist.classList.add('mist--open');
+        modalOpen();
         return;
       }
       target = target.parentNode;
     }
-  }
+  });
 }
