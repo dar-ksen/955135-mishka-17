d1ced063001d85eaab70f0ea6a443052f02b33b0
Dar <ksen.vl.set@gmail.com>
2019-08-10 10:10:37 +0300

    Исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,6 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
-var cheerio = require('gulp-cheerio');
-var replace = require('gulp-replace');
-
 var terser = require("gulp-terser");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
@@ -27,11 +24,8 @@ var server = require("browser-sync").create();
 gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
     "source/js/**"
-  ], {
-	  base: "source"
-  })
+  ], {base: "source"})
   .pipe(gulp.dest("build"));
 });
 
@@ -42,39 +36,29 @@ gulp.task("clean", function() {
 
 //Оптимизировать картинки
 gulp.task("imagemin", function() {
-  return gulp.src("build/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"));
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img"));
 });
 
 //Перевести картинки в формат webp
 gulp.task("webp", function() {
-  return gulp.src("build/img/**/*.{png,jpg}")
+  return gulp.src("source/img/**/*.{png,jpg}")
 	.pipe(webp({quality: 90}))
 	.pipe(gulp.dest("build/img"));
 });
 
 //Собрать sprite
 gulp.task("sprite", function() {
-  return gulp.src("build/img/sprite-*.svg")
-  //очищаем от fill
-  .pipe(cheerio({
-    run: function ($) {
-      $('[fill]').removeAttr('fill');
-    },
-    parserOptions: { xmlMode: true }
-  }))
-  //исправляем возможные ошибки
-  .pipe(replace('&gt;', '>'))
-  .pipe(svgstore({
-	inlineSvg: true
-	}))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+  return gulp.src("source/img/sprite/*.svg")
+  .pipe(imagemin([imagemin.svgo()]))
+  .pipe(svgstore({inlineSvg: true}))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
 });
 
 //Собрать css и минимизировать
@@ -83,9 +67,7 @@ gulp.task("css", function () {
   .pipe(plumber())
   .pipe(sourcemap.init())
   .pipe(less())
-  .pipe(postcss([
-    autoprefixer()
-  ]))
+  .pipe(postcss([autoprefixer()]))
   .pipe(minify())
   .pipe(rename("style.min.css"))
   .pipe(sourcemap.write("."))
@@ -96,29 +78,19 @@ gulp.task("css", function () {
 //Минифицировать js
 gulp.task("jsmin", function () {
   return gulp.src("source/js/*.js")
-    .pipe(terser())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest("build/js"));
+  .pipe(terser())
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulp.dest("build/js"));
 });
 
-//Собрать html
+//Собрать и минимизировать html
 gulp.task("html", function() {
   return gulp.src("source/*.html")
-    .pipe(posthtml([
-	  include()
-	  ]))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest("build"))
-    .pipe(server.stream());
+  .pipe(posthtml([include()]))
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest("build"))
+  .pipe(server.stream());
 });
-
-// //минимизировать html
-// gulp.task("htmlmin", function() {
-//   return gulp.src("build/*.html")
-//     .pipe(htmlmin({collapseWhitespace: true}))
-//     .pipe(gulp.dest("build"));
-// });
-
 
 //Запустить сервер
 gulp.task("server", function () {
@@ -134,6 +106,6 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "jsmin", "imagemin", "webp", "sprite", "html"));
+gulp.task("build", gulp.series("clean", gulp.parallel("copy", "css", "jsmin", "imagemin", "webp", "sprite"), "html"));
 
 gulp.task("start", gulp.series("build", "server"));
