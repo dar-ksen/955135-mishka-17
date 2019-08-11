5f8d5bd6ec5dceaf1d83783508c793d0138da65c
Dar <ksen.vl.set@gmail.com>
2019-08-11 11:08:45 +0300

    Исправление ошибок карты

--- source/js/map.js
+++ source/js/map.js
@@ -1,9 +1,13 @@
-var map;
-var image = 'img/icon-map-pin.svg'
+let map;
+let image = 'img/icon-map-pin.svg';
+let mapContainer = document.querySelector('.contacts__map');
+
+mapContainer.classList.remove('contacts__map--nojs');
+
 function initMap() {
   var coordinates = {lat: 59.9387782, lng: 30.3231967};
 
-  map = new google.maps.Map(document.querySelector('.contacts__map'), {
+  map = new google.maps.Map(mapContainer, {
     center: coordinates,
     zoom: 18
   });
