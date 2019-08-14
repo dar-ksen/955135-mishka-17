b54f1f759ff4ad7c4425ed1f65f06315eb841397
Dar <ksen.vl.set@gmail.com>
2019-08-14 10:46:17 +0300

    Исправление

--- source/js/map.js
+++ source/js/map.js
@@ -6,7 +6,6 @@ mapContainer.classList.remove('contacts__map--nojs');
 
 function initMap() {
   var coordinates = {lat: 59.9387782, lng: 30.3231967};
-
   map = new google.maps.Map(mapContainer, {
     center: coordinates,
     zoom: 18
